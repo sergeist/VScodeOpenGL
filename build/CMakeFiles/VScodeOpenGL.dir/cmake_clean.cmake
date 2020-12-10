9f555dd8c1053f303f90a1898815368f60f532e5
SERGEI <sergeitimofeev76@gmail.com>
2020-12-10 15:14:30 +0300

    добавлен новый вариант в

---
+++ build/CMakeFiles/VScodeOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VScodeOpenGL.dir/src/main.cpp.o"
+  "VScodeOpenGL"
+  "VScodeOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VScodeOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sergei/SimpleCoding/VScodeOpenGL
+# Build directory: /home/sergei/SimpleCoding/VScodeOpenGL/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
